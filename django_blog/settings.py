004398640663eae3d6d26eb488278b8283b2f4ae
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-02-21 18:27:44 +1000

    replaced secret key and debug mode with .env file

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
+import dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,11 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(bi8=oy3ynb*=085t!sb@f5+@@=ydy(s-po@c_wip4-l5v@5tj'
+# Load config file
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Update secret key and debug mode
+SECRET_KEY = os.environ['SECRET_KEY']
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
