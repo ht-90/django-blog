cee4cdbab257375910a8b8bbeb53e289b028bd8b
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-02-21 18:57:37 +1000

    added about page and view

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),  # direct request to website.urls
+    path('about/', include('website.urls')),
 ]
--- website/views.py
+++ website/views.py
@@ -5,3 +5,8 @@ from django.views.generic import TemplateView
 # Top page
 class IndexView(TemplateView):
     template_name = "index.html"
+
+
+# About page
+class AboutView(TemplateView):
+    template_name = "about.html"
