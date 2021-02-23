705d2f1130caf02df5dbb7c670ae2ba22be46245
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-02-23 21:22:31 +1000

    changed database to be specified from .env

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 import dotenv
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +31,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = [
-    'django-ht-90.herokuapp.com'
+    '127.0.0.1',
+    'django-ht-90.herokuapp.com',
 ]
 
 
@@ -83,10 +85,7 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600),
 }
 
 
