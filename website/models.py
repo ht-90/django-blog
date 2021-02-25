76b1a3e7df385175c97fbf6ab545b552fc5ae2cf
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-02-25 22:01:17 +1000

    added models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=True,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=True,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    created = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
+        blank=True,
+        null=True,
+    )
+    updated = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
+        blank=False,
+        null=True,
+    )
+    title = models.CharField(
+        max_length=255,
+        blank=False,
+        null=True,
+    )
+    body = models.TextField(
+        blank=True,
+        null=True,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    tag = models.ManyToManyField(
+        Tag,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.title
