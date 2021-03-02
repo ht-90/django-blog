f01709b8c1b6c6ac58a2bf74d7015ed13e1a9ab0
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-03-02 21:14:59 +1000

    added user admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from . import models
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.admin import AdminSite
+
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -15,3 +18,23 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
     pass
+
+
+# Admin page of user's
+class BlogAdminSite(AdminSite):
+    site_header = 'My Page'
+    site_title = 'My Page'
+    index_title = 'Home'
+    # Hide site url link on navbar
+    site_url = None
+
+    # Allow user to access User Admin page
+    login_form = AuthenticationForm
+    def has_permission(self, request):
+        return request.user.is_active
+
+# Create an instance and add objects to admin page
+mypage_site = BlogAdminSite(name="mypage")
+mypage_site.register(models.Post)
+mypage_site.register(models.Category)
+mypage_site.register(models.Tag)
