0159ae5e787c6e97409f935de123018fca72daa2
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-03-06 20:12:26 +1000

    added views

--- sample/urls.py
+++ sample/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import Index
+from .views import Index, CSVView, PDFView, PeopleAPIView
 
 
 urlpatterns = [
     path('', Index.as_view(), name="sample"),
+    path('csv/', CSVView.as_view(), name="csv"),
+    path('pdf/', PDFView.as_view(), name="pdf"),
+    path('api/people/', PeopleAPIView.as_view(), name="people"),
 ]
